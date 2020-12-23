246354fbf1097184cb271989d1b7aac871e2a438
NathanKassab <46613166+NathanKassab@users.noreply.github.com>
2020-12-23 13:28:23 -0500

    Block code stuff

--- minecraft/info/spicyclient/blockCoding/ui/CreateModule.java
+++ minecraft/info/spicyclient/blockCoding/ui/CreateModule.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 import org.lwjgl.input.Keyboard;
 
 import info.spicyclient.SpicyClient;
-import info.spicyclient.blockCoding.CustomModule;
-import info.spicyclient.chatCommands.Command;
-import info.spicyclient.modules.Module;
 import info.spicyclient.modules.player.BlockCoding;
-import info.spicyclient.settings.KeybindSetting;
-import info.spicyclient.settings.Setting;
 import info.spicyclient.ui.customOpenGLWidgets.Button;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
@@ -35,6 +30,8 @@ public class CreateModule extends GuiScreen {
 		
 	}
 	
+	public Button createNewModule = new Button((this.width / 2) + 30, 100, (this.width / 2) + 130, 130, 0xff202225, 0xff7289da, -1, 2, this);
+	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		
@@ -42,19 +39,43 @@ public class CreateModule extends GuiScreen {
 		
 		Gui.drawRect(0, 0, this.width, this.height, 0x9f000000);
 		
-		int factor = 10, textscale = 3;
+		int textscale = 2;
 		
-		Gui.drawRect(sr.getScaledWidth() / factor, sr.getScaledHeight() / factor, sr.getScaledWidth() - (sr.getScaledWidth() / factor), sr.getScaledHeight() - (sr.getScaledHeight() / factor), 0xff36393f);
+		Gui.drawRect(sr.getScaledWidth(), sr.getScaledHeight(), sr.getScaledWidth() - (sr.getScaledWidth()), sr.getScaledHeight() - (sr.getScaledHeight()), 0xff36393f);
 		
 		GlStateManager.pushMatrix();
 		GlStateManager.scale(textscale, textscale, 1);
-		mc.fontRendererObj.drawString("Create a module", ((sr.getScaledWidth() / 2) - ((mc.fontRendererObj.getStringWidth("Create a module") * textscale) / 2)) / textscale, ((sr.getScaledHeight() / factor) + 10) / textscale, 0xffffffff);
+		drawCenteredString(mc.fontRendererObj, "Create or edit a module", (this.width / 2) / textscale, 20 / textscale, -1);
 		GlStateManager.popMatrix();
 		
+		createNewModule = new Button((this.width / 2) + 30 + 40, 130, (this.width / 2) + 180 + 40, 90, 0xff202225, 0xff7289da, -1, 2, this);
+		createNewModule.setText("Create new module");
+		createNewModule.textScale = 1.4f;
+		
+		int buttonHoverExpand = 2;
+		
+		// For the create new module button
+		if (mouseX > (this.width / 2) + 30 + 40 && mouseX < (this.width / 2) + 180 + 40 && mouseY < 130 && mouseY > 90) {
+			createNewModule.insideColor = 0xff4d5c91;
+			createNewModule.setLeft((float) (createNewModule.getLeft() - buttonHoverExpand));
+			createNewModule.setUp((float) (createNewModule.getUp() - buttonHoverExpand));
+			createNewModule.setRight((float) (createNewModule.getRight() + buttonHoverExpand));
+			createNewModule.setBottom((float) (createNewModule.getBottom() + buttonHoverExpand));
+		}
+		
+		drawRect((this.width / 2) + 30 + 30, (this.height / 2) - 130, this.width, this.height, 0xff202225);
+		
+		createNewModule.draw();
 	}
 	
 	@Override
 	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+		
+		if (mouseX > (this.width / 2) + 30 + 40 && mouseX < (this.width / 2) + 180 + 40 && mouseY < 130 && mouseY > 90 & mouseButton == 0) {
+			
+			mc.displayGuiScreen(new BlockCodingMainUi("New module"));
+			
+		}
 		
 	}
 	
